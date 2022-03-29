59772f1b1a1d7a8b5a78d812137c6c16ea348e30
Charles <karlosstein@gmail.com>
2022-03-29 12:51:23 +0200

    mail working

--- Controllers/APIOrderController.cs
+++ Controllers/APIOrderController.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using backend.Data;
 using backend.Models;
+using System.Net.Mail;
+using System.Net;
 
 namespace backend.Controllers
 {
@@ -73,6 +75,8 @@ namespace backend.Controllers
             _context.Order.Add(OrderPostRequest.Order);
             await _context.SaveChangesAsync();
 
+            var mailProducts = "";
+
             // Behöver skriva produkter till tabell med Id för order 
             foreach (var item in OrderPostRequest.OrderProducts)
             {
@@ -80,6 +84,8 @@ namespace backend.Controllers
                 item.OrderId = OrderPostRequest.Order.Id;
                 //Behöver hämta in produkt till prissättning
                 var product = await _context.Product.FindAsync(item.ProductId);
+
+
                 // Lägg moms till moms total
                 var newVat = (product.Vat / 100) * product.Price;
                 if (product.Discount != 0)
@@ -117,6 +123,9 @@ namespace backend.Controllers
                 //Add ProductNumber from DB
                 var Product = await _context.Product.FindAsync(item.ProductId);
                 item.ProductNumber = Product.ProductNumber;
+
+                mailProducts += "<tr><td>" + product.Name + "</td><td>" + product.ProductNumber + "</td><td>" + item.ProductColor + "</td><td>" + item.ProductSize + "</td><td>" + item.Price + "</td></tr>";
+
                 _context.OrderProducts.Add(item);
                 await _context.SaveChangesAsync();
             }
@@ -131,6 +140,32 @@ namespace backend.Controllers
 
             _context.Order.Update(OrderPostRequest.Order);
             await _context.SaveChangesAsync();
+
+
+            MailMessage msg = new MailMessage();
+
+            msg.From = new MailAddress("bashpoddenxjob@gmail.com");
+            msg.To.Add(OrderPostRequest.Order.CustomerMail);
+            msg.Subject = "BASHPODDEN - Din Beställning:" + OrderPostRequest.Order.ReceiptNumber + " - " + OrderPostRequest.Order.Timestamp;
+            msg.Body = "<h1>Hej " + OrderPostRequest.Order.CustomerFirstName + " " + OrderPostRequest.Order.CustomerLastName + "!</h1><br><p>Här kommer en bekräftelse på din beställning med kvittonummer:" + OrderPostRequest.Order.ReceiptNumber + "</p><table style='width:100%; text-align:left;'><thead><tr><th>Produkt</th><th>Artikel Nummer</th><th>Färg</th><th>Storlek</th><th>Pris</th></tr></thead><tbody>" + mailProducts + "<tr><td></td><td></td><td></td><td style='text-align:right;' >Summa:</td><td>" + OrderPostRequest.Order.PriceTotal + "</td></tr><tr><td></td><td></td><td></td><td style='text-align:right;'>Varav Moms:</td><td>" + OrderPostRequest.Order.VatTotal + "</td></tr></tbody></table><p>Observera att detta mail också gäller som kvitto på din beställning</p><ul><li>Beställare: " + OrderPostRequest.Order.CustomerFirstName + " " + OrderPostRequest.Order.CustomerLastName + "</li><li>Telefonnummer: " + OrderPostRequest.Order.CustomerPhone + "</li><li>E-mail: " + OrderPostRequest.Order.CustomerMail + "</li><li>Kvittonummer: " + OrderPostRequest.Order.ReceiptNumber + "</li><li>Ordernummer: " + OrderPostRequest.Order.OrderNumber + "</li></ul><ul><li>Bashpodden</li><li>Gatan 3, 137 19, Långtbortistan</li><li>Bankgiro: 9845-789</li><li>" + OrderPostRequest.Order.Timestamp + "</li></ul><h2>Hälsningar Bashpodden</h2>";
+            msg.IsBodyHtml = true;
+            //msg.Priority = MailPriority.High;
+
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.EnableSsl = true;
+                client.UseDefaultCredentials = false;
+                client.Credentials = new NetworkCredential("bashpoddenxjob@gmail.com", "HH1");
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                client.Send(msg);
+            }
+
+
+
             return CreatedAtAction("GetOrder", new { id = OrderPostRequest.Order.Id }, _context.Order);
         }
 
