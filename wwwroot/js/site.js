a1a6dfb9cfaa396790551c6e668ccd420425a1f9
Charles <karlosstein@gmail.com>
2022-04-10 12:08:15 +0200

    privacy och fixed meny

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let sideMenu = document.getElementById("side-menu");
+let toggleButton = document.getElementById("toggle-button");
+
+toggleButton.addEventListener("click", () => {
+  toggleSideMenu();
+});
+sideMenu.addEventListener("click", () => {
+  toggleSideMenu();
+});
+
+const toggleSideMenu = () => {
+  sideMenu.classList.toggle("open-side-menu");
+  if (sideMenu.classList.contains("open-side-menu")) {
+    toggleButton.style.backgroundColor = "#ff000030";
+  } else {
+    toggleButton.style.backgroundColor = "#00ff0030";
+  }
+};
+
+window.onload = () => {
+  toggleButton.style.backgroundColor = "#00ff0030";
+};
